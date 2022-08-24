89faadb24d15c17d3dbc28af2a50a02d9bf5ad9c
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2022-08-24 14:04:54 +0200

    Register form looking neat

--- network/network/settings.py
+++ network/network/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+#STATIC_ROOT = ''
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../frontend/build/static'),
-    os.path.join(BASE_DIR, '../network/static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 # Default primary key field type
