c59ea2fa8fb900399c9503a7fd34fbdab4db99d6
piotr-bledowski <piotr.bledowski.77@gmail.com>
2022-10-05 18:55:39 +0200

    Fixed id's and screen darkener's stacking, dialog itself is still a mess

--- frontend/src/components/DeleteDialog.jsx
+++ frontend/src/components/DeleteDialog.jsx
@@ -1,32 +1,29 @@
 import { hideDialog } from "../utils/helpers";
 
 
-const DeleteDialog = ({uri, type}) => {
+const DeleteDialog = ({uri, type, id}) => {
 
     const handleClickYes = () => {
         fetch(uri, {method: 'DELETE'});
-        hideDialog();
+        hideDialog(id);
     }
 
     const handleClickNo = () => {
-        hideDialog();
+        hideDialog(id);
     }
 
     return (
-        <>
-            <div id="screen-darkener" className="screen-darkener"></div>
-            <div id="dialog-div" className="dialog-div">
-                <h3>Are you sure you want to delete this?</h3>
-                <div className="dialog-btns">
-                    <button onClick={handleClickYes}>
-                        <h4>Yes</h4>
-                    </button>
-                    <button onClick={handleClickNo}>
-                        <h4>No</h4>
-                    </button>
-                </div>
+        <div id={`dialog-div-${id}`} className="dialog-div">
+            <h3>Are you sure you want to delete this {type}?</h3>
+            <div className="dialog-btns">
+                <button onClick={handleClickYes}>
+                    <h4>Yes</h4>
+                </button>
+                <button onClick={handleClickNo}>
+                    <h4>No</h4>
+                </button>
             </div>
-        </>
+        </div>
     )
 }
 
